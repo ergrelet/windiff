180b37efe9f12ba9f7b5da0469a13aa6926dbde1
ergrelet <ergrelet@users.noreply.github.com>
2023-05-28 17:03:10 +0200

    Use a pre-built version of windiff_cli to build the frontend

--- ci/build_frontend.sh
+++ ci/build_frontend.sh
@@ -3,12 +3,14 @@
 set -x
 set -e
 
+WINDIFF_CLI_URL=https://github.com/ergrelet/windiff/releases/download/v1.0.0/windiff_cli-x86_64-unknown-linux-musl
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
+# Download a pre-built version of `windiff_cli`
+wget -O windiff_cli ${WINDIFF_CLI_URL} && chmod +x windiff_cli
+
 # Generate databases
-cd "$PROJECT_ROOT/windiff_cli"
-cargo build --release
-./target/release/windiff_cli "$PROJECT_ROOT/ci/db_configuration.json" "$PROJECT_ROOT/windiff_frontend/public/" --low-storage-mode
+./windiff_cli "$PROJECT_ROOT/ci/db_configuration.json" "$PROJECT_ROOT/windiff_frontend/public/" --low-storage-mode
 
 # Build the frontend
 cd "$PROJECT_ROOT/windiff_frontend"
